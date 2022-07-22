124c30395739e74b5134504c8a4063ef9783fd81
massije <jedc.marti@gmail.com>
2022-07-22 11:32:35 +0200

    05-Section-Finished

---
+++ 05-Section/01-Hello-World/02-C++/miPrimerPrograma.cpp
@@ -0,0 +1,15 @@
+ /* #include <iostream.h>  esto ya no funciona porque algunas plataformas no lo soportan, por lo que tendremos que quitar la h*/
+ #include <iostream>
+ using namespace std;
+
+int main (void){
+
+    /*Esto nos permite hacer comentarios*/
+    /* \n esto nos permite hacer una salto de linea*/
+    cout << "This is my first program.\n";
+    cout << "Pulse Intro para finalizar...";
+    cin.get();
+
+    return 0;
+
+}
