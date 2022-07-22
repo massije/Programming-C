50dff830f3ee6a2651236f5b5c9e945a867c4cd8
massije <jedc.marti@gmail.com>
2022-07-22 10:59:29 +0200

    Finished

--- 05-Section/01-Hello-World/miPrimerPrograma.c
+++ 05-Section/01-Hello-World/miPrimerPrograma.c
@@ -1,1 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int  main(void){
+
+    printf("This is my first program!\n");
+    printf("Pulse Intro para finalizar...");
+    getchar();
+
+    return 0;
+}
